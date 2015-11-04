818987d11653c5081b9c9f23eda7f6aea177811f
Andrey B <justandreyb@gmail.com>
2015-11-04 19:34:33 +0300

    04.11.2015
    1_6 : done
    1_6_1 : done

--- 1_6.c
+++ 1_6.c
@@ -2,20 +2,23 @@
  
 #include <stdio.h>
 #include <conio.h>
+#include <string.h>
+#define FILECONTAIN 1000
+#define FILENAMELEN 100
 
 void main (void) {
-	char name[15];
+
 	FILE *in_file, *out_file;
-    char in_filename[100];
+    char in_filename[FILENAMELEN];
  
 	printf("Enter the filename to open: ");
-    fgets(in_filename, 100, stdin ); 
-    char i;
-    for ( i = 0; i < 100; i++ )
+    fgets(in_filename, FILENAMELEN, stdin ); 
+    char count;
+    for ( count = 0; count < FILENAMELEN; count++ )
     {
-        if (in_filename[i] == '\n' )
+        if (in_filename[count] == '\n' )
         {
-            in_filename[i] = '\0';
+            in_filename[count] = '\0';
             break;
         }
     }
@@ -24,21 +27,23 @@ void main (void) {
 		printf("Error in open/create file\n");
 	else 
 	{	
-		printf("Enter your name : ");
-		fflush(stdin);
-		fgets(name, 15, stdin ); 
-    	for ( i = 0; i < 100; i++ )
-    	{
-	        if (name[i] == '\n' )
-	        {
-	            name[i] = '\0';
-	            break;
-        	}
-    	}
-		fprintf(file, "%s\n", name);
-	}
-	fclose(file);
+		char Infile[FILECONTAIN];
+		while(!feof(in_file))
+			fgets(Infile, FILECONTAIN, in_file);	
+		fclose(in_file); 
+    	
+    	printf("\nLength: %d", strlen(Infile));
 
+    	if ((out_file = fopen("outputfile.txt","w")) == NULL)
+    		printf("Error in creating output file");
+    	else
+    	{
+			for (count = (strlen(Infile)-1); count >= 0; count--)
+				fprintf(out_file, "%c", Infile[count]);
+			fclose(out_file);
+			printf("\nDone !");
+		}
+	}
 	return;
 
 } 
--- 1_6_1.c
+++ 1_6_1.c
@@ -1,4 +1,77 @@
 // Записать текстовый файл F1, состоящий из произвольной последовательности
 // буквенных символов. Упорядочить символы в алфавитном порядке, при этом
 // все повторяющиеся символы должны быть удалены, и переписать текст в файл F2
-// Дан файл А. Записать в файл В компоненты файла А в в обратном порядке.
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#define FILECONTAIN 1000
+#define FILENAMELEN 100
+
+void main (void) {
+
+	FILE *in_file, *out_file;
+    char in_filename[FILENAMELEN];
+ 
+	printf("Enter the filename to open: ");
+    fgets(in_filename, FILENAMELEN, stdin ); 
+    char count;
+    for ( count = 0; count < FILENAMELEN; count++ )
+    {
+        if (in_filename[count] == '\n' )
+        {
+            in_filename[count] = '\0';
+            break;
+        }
+    }
+
+	if ((in_file = fopen(in_filename,"r")) == NULL)
+		printf("Error in open/create file\n");
+	else 
+	{	
+		char Infile[FILECONTAIN];
+		count = 0;
+		while ((Infile[count] = fgetc (in_file)) != EOF) 
+		{
+			if (Infile[count] == '\n') 
+			{
+				Infile[count] = '\0';
+				count = 0;
+			}
+			else 
+				count++;
+		}
+		Infile[count] = '\0';
+		printf("%s\n",Infile);
+		fclose(in_file); 
+    	
+    	printf("\nLength: %d", strlen(Infile));
+
+    	char count_sort;
+    	for (count_sort = 1; count_sort < strlen(Infile); count_sort++) 
+    		for (count = 1 ; count < strlen(Infile); count++)
+				if (Infile[count-1] > Infile[count])
+				{
+					Infile[count-1] = Infile[count-1] + Infile[count];
+					Infile[count] = Infile[count-1] - Infile[count];
+					Infile[count-1] = Infile[count-1] - Infile[count];
+				}
+
+    	if ((out_file = fopen("output.txt","w")) == NULL)
+    		printf("Error in creating output file");
+    	else
+    	{
+    		char flag = '\0';
+			for (count = 0 ; count < strlen(Infile); count++)
+				if (Infile[count] != flag)
+				{
+					fprintf(out_file, "%c", Infile[count]);
+					flag = Infile[count];
+				}
+			fclose(out_file);
+			printf("\nDone !");
+		}
+	}
+	return;
+
+} 
